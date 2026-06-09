d7fbe7006cec3362f7357ca5254968d6fe23a4a4
Prajjwal Singh <singhshanu33022@gmail.com>
2026-06-10 04:48:19 +0530

    Fix model loading

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ from transformers import (
 def load_soil_model():
 
     tokenizer = T5Tokenizer.from_pretrained(
-            "soil_model"
+        "t5-small"
     )
 
     model = T5ForConditionalGeneration.from_pretrained(
-        "soil_model"
+        "t5-small"
     )
 
     return tokenizer, model
